d1e4c9bd307da864d7b3ec21de74d686272fb46e
Marcelo Fortunato <mfortunato@PDM-00887.local>
2019-05-12 10:25:41 +0100

    change(favorite): added verify to before add a repo in list

--- src/store/sagas/favorites.js
+++ src/store/sagas/favorites.js
@@ -1,4 +1,4 @@
-import { call, put } from 'redux-saga/effects'
+import { call, put, select } from 'redux-saga/effects'
 import api from '../../services/api'
 
 import { addFavoriteSuccess, addFavoriteFailure } from '../actions/favorites'
@@ -7,14 +7,19 @@ export function* addFavorite(action) {
     try {
         const { data } = yield call(api.get, `/repos/${action.payload.repository}`)
 
-        const repositoryData = {
-            id: data.id,
-            name: data.full_name,
-            description: data.description,
-            url: data.html_url,
-        }
+        const isDuplicate = yield select(state => state.favorites.data.find(favorite => favorite.id === data.id))
 
-        yield put(addFavoriteSuccess(repositoryData))
+        if (isDuplicate) {
+            yield put(addFavoriteFailure('Repositório Duplicado'))
+        } else {
+            const repositoryData = {
+                id: data.id,
+                name: data.full_name,
+                description: data.description,
+                url: data.html_url,
+            }
+            yield put(addFavoriteSuccess(repositoryData))
+        }
     } catch (err) {
         yield put(addFavoriteFailure('Error add repo'))
     }
